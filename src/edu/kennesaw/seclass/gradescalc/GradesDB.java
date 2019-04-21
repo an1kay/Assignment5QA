bc6f8789c2575761d1b0d1c6c44bb65f544a57f9
steve anderson <stevo.7401@gmail.com>
2019-04-20 22:04:07 -0400

    Basic xlsx importing created, no files imported currently. basic exception handling added for workheet = null. Libraries added for importing from workbooks.

--- src/edu/kennesaw/seclass/gradescalc/GradesDB.java
+++ src/edu/kennesaw/seclass/gradescalc/GradesDB.java
@@ -1,19 +1,91 @@
 package edu.kennesaw.seclass.gradescalc;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class GradesDB {
-	
-	GradesDB(String gradeSheet){
-		
+
+	private HashSet<Student> students = new HashSet();
+
+	GradesDB(String path) throws Exception {
+		//imports grade sheet based on a path
+		String studentInfoSheet = "StudentsInfo";
+		File DBFile = new File(path);
+		XSSFWorkbook workBook;
+		XSSFSheet workSheet = null;
+		try {
+			FileInputStream fis = new FileInputStream(DBFile);
+			workBook = 	ExtractWorkbook(fis);
+			workSheet = ExtractSheet(workBook, studentInfoSheet);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		if (workSheet != null	)
+		{
+			ExtractStudents(workSheet);
+
+		} else
+		{
+			throw new NullPointerException("WorkSheet is Empty, Check path");
+		}
+
+
+
 	}
+
+	private XSSFWorkbook ExtractWorkbook(FileInputStream fis) {
+		XSSFWorkbook workBook = null;
+		try {
+			workBook = new XSSFWorkbook(fis);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return workBook;
+	}
+
+	//workbook helper methods
+	private XSSFSheet ExtractSheet(XSSFWorkbook wb, String sheetName)
+	{//purpose of method is to successfully extract sheets needed
+		XSSFSheet workSheet = null;
+
+		workSheet = wb.getSheet(sheetName);
+
+		return workSheet;
+	}
+
+	private void ExtractStudents(XSSFSheet workSheet)
+	{
+		Iterator<Row> RowIterator = workSheet.iterator();
+	}
+
+
+
+
+
+
+
+
+
+
 	
 	public int getNumStudents() {
+		// counts the number of students in the file and returns the resulting number
 		return 0;		
 		
 	}
 	
 	public int getNumAssignments() {
+		//counts the number of
 		return 0;
 		
 	}
